425c7ef07525772af2111bf3daf94d598f55e944
chris_chang <chris_chang@gemteks.com>
2019-02-12 17:29:03 +0800

    [master][LoraRepeater][feature] To remove log in update.sh

--- update.sh
+++ update.sh
@@ -6,5 +6,3 @@ cp lora_repeater_starter /etc/init.d/
 chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
 update-rc.d lora_repeater_starter remove;
 update-rc.d lora_repeater_starter defaults 99;
-
-echo "Finished: lora_repeater_stater start or reboot wil be run";
