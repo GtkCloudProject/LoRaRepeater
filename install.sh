cd3f0812efbec9a65a85f14a35419a79c25658b3
chris_chang <chris_chang@gemteks.com>
2019-02-12 17:04:11 +0800

    [master][LoraRepeater][feature] To change the install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,8 @@ echo "To add system id 05 and key to database"
 mysql -uroot -p123456 lora -e "insert into table_netid values ('05','05000000','05ffffff','1000','BA21C6216312C334597D88711D9EFABE','BA21C6216312C334597D88711D9EFABE');"
 
 echo "To install lora_repeater packages"
-cd /mnt/data
-git clone https://github.com/GtkCloudProject/LoRaRepeater.git
-cd LoRaRepeater
+cd /mnt/data/LoRaRepeater
+git pull --rebase
 cp lora_repeater_starter /etc/init.d/
 chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
 update-rc.d lora_repeater_starter remove;
