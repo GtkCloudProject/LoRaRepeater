e88a117f0de6aeadd70c417a380571639f743fb0
chris_chang <chris_chang@gemteks.com>
2019-02-12 17:25:04 +0800

    [master][LoraRepeater][feature] To modify install.sh and update.sh for downloading codes frome git server

--- install.sh
+++ install.sh
@@ -11,11 +11,6 @@ echo "To add system id 05 and key to database"
 mysql -uroot -p123456 lora -e "insert into table_netid values ('05','05000000','05ffffff','1000','BA21C6216312C334597D88711D9EFABE','BA21C6216312C334597D88711D9EFABE');"
 
 echo "To install lora_repeater packages"
-cd /mnt/data/LoRaRepeater
-git pull --rebase
-cp lora_repeater_starter /etc/init.d/
-chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
-update-rc.d lora_repeater_starter remove;
-update-rc.d lora_repeater_starter defaults 99;
+sh /mnt/data/LoRaRepeater/update.sh
 
 echo "Finished: lora_repeater_stater start or reboot wil be run";
--- update.sh
+++ update.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
-git fetch;
-git pull origin master:master;
-cp inq.py deq.py /usr/local/bin/;
-cp lora_repeater_starter /etc/init.d/;
-chmod 755 /usr/local/bin/inq.py /usr/local/bin/deq.py /etc/init.d/lora_repeater_starter;
-mv /usr/local/bin/inq.py /usr/local/bin/inq; mv /usr/local/bin/deq.py /usr/local/bin/deq;
+echo "To install lora_repeater packages"
+cd /mnt/data/LoRaRepeater
+git pull --rebase
+cp lora_repeater_starter /etc/init.d/
+chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
 update-rc.d lora_repeater_starter remove;
-update-rc.d lora_repeater_starter defaults;
+update-rc.d lora_repeater_starter defaults 99;
 
 echo "Finished: lora_repeater_stater start or reboot wil be run";
