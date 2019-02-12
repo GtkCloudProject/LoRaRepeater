ddbba2fb756ebc739a8b2f248c619d674ddb693e
chris_chang <chris_chang@gemteks.com>
2019-02-12 17:33:59 +0800

    [master][LoraRepeater][feature] To modify install.sh and remove update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "To install lora_repeater packages"
-cd /mnt/data/LoRaRepeater
-git pull --rebase
-cp lora_repeater_starter /etc/init.d/
-chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
-update-rc.d lora_repeater_starter remove;
-update-rc.d lora_repeater_starter defaults 99;
