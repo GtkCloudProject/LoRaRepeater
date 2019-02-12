274178fe59c8630f9458f1d1f46a32866b395f91
chris_chang <chris_chang@gemteks.com>
2019-02-12 17:43:06 +0800

    [master][LoraRepeater][feature] To modify install.sh for add -f for removing lora_repeater_starter by update-rc.d

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "To install lora_repeater packages"
 cd /mnt/data/LoRaRepeater
 cp lora_repeater_starter /etc/init.d/
 chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
-update-rc.d lora_repeater_starter remove;
+update-rc.d -f lora_repeater_starter remove;
 update-rc.d lora_repeater_starter defaults 99;
 
 echo "Finished: lora_repeater_stater start or reboot wil be run";
