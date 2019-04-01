56dd14e1ad1357dd614a50a97a6135f88efe7d86
GtkCloudProject <47553198+GtkCloudProject@users.noreply.github.com>
2019-04-01 15:15:17 +0800

    [master][LoraRepeater][feature] add back up to zip

--- back_up_DB.sh
+++ back_up_DB.sh
@@ -2,4 +2,7 @@
 
 echo "back up mysql sensor database sensordata table"
 mysqldump -uroot -p123456 sensor sensordata > /mnt/data/Ftpdir/sensordata.sql
+cd /mnt/data/Ftpdir
+zip sensordata.zip sensordata.sql
+rm sensordata.sql
 echo "back up done!"
