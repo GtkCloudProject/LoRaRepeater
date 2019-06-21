fe4b965a65b3eb131b8e6fe2a2d506ea1199fd97
chris_chang <chris_chang@gemteks.com>
2019-06-21 17:12:28 +0800

    [master][LoraRepeater][feature] To fix the issue that LoRa Repeater related daemons can't run after upgrading FW offline

--- check_repeater.sh
+++ check_repeater.sh
@@ -29,12 +29,19 @@ do
     file="repeater_upgrade.zip"
     cd /mnt/data/Ftpdir
     if [ -f "$file" ];then
-    echo "Find upgrade FW"
-    #killall check_repeater.sh
-    killall inq.py deq.py
-    unzip -o /mnt/data/Ftpdir/repeater_upgrade.zip -d /mnt/data/LoRaRepeater
-    rm repeater_upgrade.zip
-    #source /mnt/data/LoRaRepeater/install.sh
-    reboot
+        echo "Find upgrade FW"
+        #killall check_repeater.sh
+        killall inq.py deq.py
+        unzip -o /mnt/data/Ftpdir/repeater_upgrade.zip -d /mnt/data/LoRaRepeater
+        rm repeater_upgrade.zip
+
+        #To setup starting daemon after booting
+        cd /mnt/data/LoRaRepeater
+        cp lora_repeater_starter /etc/init.d/
+        chmod 755 /mnt/data/LoRaRepeater/inq.py /mnt/data/LoRaRepeater/deq.py /mnt/data/LoRaRepeater/check_repeater.sh /etc/init.d/lora_repeater_starter
+        update-rc.d -f lora_repeater_starter remove;
+        update-rc.d lora_repeater_starter defaults 99;
+
+        reboot
     fi
 done
