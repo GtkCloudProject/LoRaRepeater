7eda310a33c9e7fc17e04fcb825c9509ee33d34b
chris_chang <chris_chang@gemteks.com>
2019-06-24 06:10:19 +0800

    [master][LoraRepeater][feature] To add change ip script for following LoRa Repeater
        05001001, 05001002, 05001003, 05001004, 05001005, 05001006,
        05001007, 05001008, 05001009, 0500100a, 05002001, 05002002,
        05002003, 05002004, 05003001, 05003002, 05004001

---
+++ tool/change_ip/ch_ip_05003001.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+App_Server_IP="10.56.149.102"
+Diag_PC_IP="10.56.149.192"
+NPort_IP="10.56.149.193"
+
+#To change the IP of inq.py
+sed -i 's/10.56.147.176/"'$App_Server_IP'"/g' /mnt/data/LoRaRepeater/inq.py
+sed -i 's/10.56.147.240/"'$Diag_PC_IP'"/g' /mnt/data/LoRaRepeater/inq.py
+sed -i 's/10.56.147.241/"'$NPort_IP'"/g' /mnt/data/LoRaRepeater/inq.py
+
+#To change the IP of deq.py
+sed -i 's/10.56.147.176/"'$App_Server_IP'"/g' /mnt/data/LoRaRepeater/deq.py
+sed -i 's/10.56.147.240/"'$Diag_PC_IP'"/g' /mnt/data/LoRaRepeater/deq.py
+sed -i 's/10.56.147.241/"'$NPort_IP'"/g' /mnt/data/LoRaRepeater/deq.py
+
+echo "05004001 is changed the IP Setting successfully, as following:"
+echo "  - Application SERVER IP : $App_Server_IP"
+echo "  - Diagnosis PC IP : $Diag_PC_IP"
+echo "  - NPort 5450 IP : $NPort_IP"
