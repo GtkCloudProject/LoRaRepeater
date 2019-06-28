e292618b6c4767f4d6d2e095a24ab73da12a9f8d
GtkCloudProject <47553198+GtkCloudProject@users.noreply.github.com>
2019-06-28 13:04:58 +0800

    [master][LoraRepeater][bug fixed] To fix the description error

--- tool/change_ip/ch_ip_05001004.sh
+++ tool/change_ip/ch_ip_05001004.sh
@@ -14,7 +14,7 @@ sed -i 's/10.56.147.176/'$App_Server_IP'/g' /mnt/data/LoRaRepeater/deq.py
 sed -i 's/10.56.147.240/'$Diag_PC_IP'/g' /mnt/data/LoRaRepeater/deq.py
 sed -i 's/10.56.147.241/'$NPort_IP'/g' /mnt/data/LoRaRepeater/deq.py
 
-echo "05004001 is changed the IP Setting successfully, as following:"
+echo "The IP Setting is successful, as following:"
 echo "  - Application SERVER IP : $App_Server_IP"
 echo "  - Diagnosis PC IP : $Diag_PC_IP"
 echo "  - NPort 5450 IP : $NPort_IP"
